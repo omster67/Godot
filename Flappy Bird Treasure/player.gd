0ea5d2d6e64de83a8a4b9c77282796d7c0a882b5
67Om <om.patel@mcvts.org>
2026-06-04 10:44:45 -0400

    Add files via upload

--- Flappy Bird Treasure/player.gd
+++ Flappy Bird Treasure/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 500
-const JUMP_VELOCITY = -600
+const SPEED = 800
+const JUMP_VELOCITY = -700
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,3 +26,5 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
